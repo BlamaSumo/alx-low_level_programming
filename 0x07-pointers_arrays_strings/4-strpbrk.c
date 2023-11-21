9d582483e51debd62967a6bf95383d7ab6310784
Blama Philip Sumo <philipblamasumo@outlook.com>
2023-11-21 09:47:09 -0800

    a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,12 +2,25 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: parameter 1
- * @accept: parameter 2
- * Return: pointer to the byte
+ * @s: string to search from
+ * @accept: string to search in s
+ * Return: accept or NULL
  */
 
 char *_strpbrk(char *s, char *accept)
 {
+	while (*s != '\0')
+	{
+		const char *a = accept;
 
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+
+	return (NULL);
 }
