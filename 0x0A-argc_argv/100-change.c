54a6e361ab1e31867dfbcfeeadfba7439e89fcb0
Blama Philip Sumo <philipblamasumo@outlook.com>
2023-11-27 08:23:07 -0800

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount
+ * main - main function
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
